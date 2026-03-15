0aef8cfb71cc56a75da6dc918de81dc017239302
s-71018 <svnargund.1806@gmail.com>
2026-03-15 22:31:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def get_consultation():
 
 if __name__ == '__main__':
     # Use port from environment variable for deployment
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
